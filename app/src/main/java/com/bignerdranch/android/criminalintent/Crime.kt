7ac634bfcf2d0beb0227d1ea5d9a231a814a9b77
Chanel Mendoza <chanelmdza@gmail.com>
2022-11-19 15:56:38 -0800

    add Crime data class and crime detail fragment layout file

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+/**
+ * Data class representing a Crime.
+ */
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
