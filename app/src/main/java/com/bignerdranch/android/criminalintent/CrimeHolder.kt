c430600800346b05e6b5729f3df33ddc76c7fa9d
Chanel Mendoza <chanelmdza@gmail.com>
2022-11-20 22:33:44 -0800

    Set up recycler view

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeHolder.kt
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+
+class CrimeHolder(
+    private val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Snackbar.make(
+                it,
+                "${crime.title} clicked~",
+                BaseTransientBottomBar.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
